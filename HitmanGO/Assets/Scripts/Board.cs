497484614a2c24f6d437ebc12ba94894152815ac
Vladislav <kill4ik98@mail.ru>
2018-04-12 02:12:15 +0300

    first commit

---
+++ HitmanGO/Assets/Scripts/Board.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour 
+{
+	public static float spacing = 2f;
+
+	public static readonly Vector2[] directions = 
+	{
+		new Vector2(spacing, 0f),
+		new Vector2(-spacing, 0f),
+		new Vector2(0f, spacing),
+		new Vector2(0f,-spacing)
+	};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HitmanGO/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	
+	float m_h;
+	public float H { get { return m_h; } }
+	float m_v;
+	public float V { get { return m_v; } }
+	public bool m_inputEnabled = false; 
+	public bool InputEnabled { get { return m_inputEnabled; } set { m_inputEnabled = value ; } }	
+
+	//plyaerInput.InputEnabled = false;
+	public void GetKeyInput() {
+		if (m_inputEnabled) {
+			m_h = Input.GetAxisRaw("Horizontal");
+			m_v = Input.GetAxisRaw("Vertical");
+		}
+	}
+
+}
